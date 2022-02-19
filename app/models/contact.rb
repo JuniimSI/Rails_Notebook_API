e206dccc049f1323bdbff8e97b651a26383cf765
JuniimSI <jrfernandesti@gmail.com>
2022-02-19 15:47:09 -0300

    feat: added nested attributes for phones

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,19 +2,19 @@
 
 class Contact < ApplicationRecord
   # Associations
-  belongs_to :kind, optional: true
+  belongs_to :kind
   has_many :phones
+
+  accepts_nested_attributes_for :phones, allow_destroy: true
 
   def birthdate_br
     I18n.l(birthdate) unless birthdate.blank?
   end
 
-  def to_br
-    {
-      name: name,
-      email: email,
-      birthdate: birthdate_br
-    }
+  def as_json(options = {})
+    h = super(options)
+    h[:birthdate] = birthdate_br
+    h
   end
 
   # def author

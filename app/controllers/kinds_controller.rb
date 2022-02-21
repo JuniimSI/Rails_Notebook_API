64840c3e200cf349f866c7fc0f58f145b2885250
JuniimSI <jrfernandesti@gmail.com>
2022-02-21 08:09:50 -0300

    feat: added new routes and improve serializers

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -39,13 +39,20 @@ class KindsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_kind
-      @kind = Kind.find(params[:id])
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_kind
+    if params[:contact_id]
+      @kind = Contact.find(params[:contact_id]).kind
+      return @kind
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def kind_params
-      params.require(:kind).permit(:description)
-    end
+    @kind = Kind.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def kind_params
+    params.require(:kind).permit(:description)
+  end
 end
+
